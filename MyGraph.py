a75fdc1b269367a6c610322939dffe8e73e125a1
Ilm Modh <ilm_modh1@baylor.edu>
2024-08-29 16:43:21 -0500

    initial commit

---
+++ MyGraph.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+
+plt.show()
+
+# 1. create your VE
+#pc py -3-m venv myvenv
+#mac python3 -m venv myvenv
+
+#2. activate your virtual environment
+#pc myvenv/scripts/activate
+#mac source myvenv/bin/activate
+
+#3 install 3rd party libraries
+#pip3 install matplotlib
+
+#version control refers to the project as a whole
+#Source control is at a smaller level
---
+++ python1.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+c = a + b
+
+for x in range(10):
+    y = x * c
+#debugger stops prior to execution
+
+message = "Hello World"
+
+print(message)
